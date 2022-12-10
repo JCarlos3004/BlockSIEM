2a3bc11fb4eb7e59d7ac00ccc749c64e1a1ca1e5
JCarlos3004 <jeanmendoza93@gmail.com>
2022-12-10 10:19:38 -0500

    Modificación Sabado 10/12

--- app.py
+++ app.py
@@ -1,30 +1,59 @@
 from flask import Flask,render_template, request
-
+import pandas as pd
+import hashlib
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index.html')
+@app.route('/index')
 def index():
     return render_template("index.html")
 
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login")
 
 @app.route('/layout')
 def layout():
     return render_template("layout.html")
 
-@app.route('/tables.html')
+@app.route('/tables')
 def tables():
     return render_template("tables.html")
 
-@app.route('/transacciones.html')
+@app.route('/transacciones')
 def transacciones():
-    return render_template("transacciones.html")
+    logs = pd.read_csv('logs.csv', sep=";")
+    logs_1 = {'n':logs["n"].to_list(),
+              'id':logs["ID"].to_list(),
+              'timestamp':logs["timestamp"].to_list(),
+              'descripcion':logs["agent.type"].to_list(),
+              'hash': [ hashlib.sha256(str(i).encode()).hexdigest()[:20]+"..." for i in range(len(logs["n"]))],
+              'clasificacion':logs["Clasificacion"].to_list(),
+              'bloques':logs["Bloque"].to_list()}
+    return render_template("transacciones.html", data=logs_1)
+x = 2
+@app.route('/blockchain', methods=['POST','GET'])
+def blockchain():
+    global x
+    x=x+1
+    if request.method=='POST':
+        bloque = request.form.get("block")
+    logs = pd.read_csv('logs.csv', sep=";")
+    logs_1 = {'n':logs["n"].to_list(),
+              'id':logs["ID"].to_list(),
+              'timestamp':logs["timestamp"].to_list(),
+              'descripcion':logs["agent.type"].to_list(),
+              'hash': [ hashlib.sha256(str(i).encode()).hexdigest()[:20]+"..." for i in range(len(logs["n"]))],
+              'hash_c':  [ hashlib.sha256(str(i).encode()).hexdigest() for i in range(len(logs["n"]))],
+              'clasificacion':logs["Clasificacion"].to_list(),
+              'bloques':logs["Bloque"].to_list(),
+              'x':x}
+
+    print(x)
+    return render_template("blockchain.html", data=logs_1)
 
 if __name__ == '__main__':
     app.run(debug=True)
